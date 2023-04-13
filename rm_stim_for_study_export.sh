0eae51cffb9c558fafc715b47648184aa2d626f0
dorian <dorian@mino.rs>
2023-04-13 16:08:15 +0100

    update script to rm stimuli to also move git fld

--- rm_stim_for_study_export.sh
+++ rm_stim_for_study_export.sh
@@ -5,6 +5,7 @@ tmpDir=$(mktemp -d "tmp.XXXXXXXXXXXX")
 mv "$tmpDir" ..
 tmpDir=../$tmpDir
 
+mv .git $tmpDir
 mv stimuli/masked_datasets $tmpDir
 mv stimuli/unmasked_datasets $tmpDir
 mv stimuli/masks $tmpDir
