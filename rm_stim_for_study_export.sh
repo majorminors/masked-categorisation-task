6051d54393d5e7bcf997e21f447eef59d443bb90
dorian <dorian@mino.rs>
2023-04-13 15:42:56 +0100

    add script to move big stim files out of the folder so we can export the study with jatos (since it can't handle big studies)

---
+++ rm_stim_for_study_export.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tmpDir=$(mktemp -d "tmp.XXXXXXXXXXXX")
+
+mv "$tmpDir" ..
+tmpDir=../$tmpDir
+
+mv stimuli/masked_datasets $tmpDir
+mv stimuli/unmasked_datasets $tmpDir
+mv stimuli/masks $tmpDir
+mv stimuli/mask_bubbles $tmpDir
